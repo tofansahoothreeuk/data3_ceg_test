32e99cedb0f229c3c85c6f99eb1a3a8f747b158c
Tofan Sahoo <tofan.sahoo@three.co.uk>
2022-08-05 18:20:44 +0000

    customdb2

--- macros/data3_generate_database_name.sql
+++ macros/data3_generate_database_name.sql
@@ -2,7 +2,7 @@
 
 {% macro generate_database_name(custom_database_name=none, node=none) -%}
 
-    {%- set default_database = {{ env_var('DBT_TARGET_DATABASE') }} -%}
+    {%- set default_database = env_var('DBT_TARGET_DATABASE') -%}
     {%- if custom_database_name is none -%}
 
         {{ default_database }}
